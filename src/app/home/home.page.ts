ae7479d1a9627b78f2cf738903acb8c786ab9668
Georgeton <georgeton.santos.santana@gmail.com>
2023-08-14 15:14:04 -0300

    feat: add capicitor-push notifications

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather/weather.service';
 import { GeolocationService } from '../services/geolocation/geolocation.service';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+import { Injectable } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -15,6 +23,45 @@ export class HomePage implements OnInit{
   
   ngOnInit() {
     this.getWeatherByLocation();
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
  
   getWeatherByLocation() {
